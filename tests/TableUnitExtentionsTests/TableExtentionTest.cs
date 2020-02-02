a3ecf9372fe6638a10bba7f3025380870da52e5c
Milovidov Vasya <milovidov.vasya@gmail.com>
2020-02-02 14:00:44 +0300

    add tests

--- tests/TableUnitExtentionsTests/TableExtentionTest.cs
+++ tests/TableUnitExtentionsTests/TableExtentionTest.cs
@@ -28,7 +28,7 @@ namespace TableUnitExtentionsTests {
 		}
 
 		[Fact]
-		public void CreateTable_AllFieldsArePresentInTheResultingObject() {
+		public void CreateTable_AllFieldsArePresentInResultingObject() {
 			var usersTable = TableExtentions.Create<Users>(Users.TableName);
 
 			var actual = usersTable.Fields.OrderBy(x => x).ToArray();
@@ -36,6 +36,51 @@ namespace TableUnitExtentionsTests {
 			var expected = typeof(Users).GetProperties().Select(prop => prop.Name).OrderBy(x => x).ToArray();
 
 			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void IncludeField_TheFieldIsIncludedInResult() {
+
+		}
+
+		[Fact]
+		public void ExcludeField_TheFieldIsExcludedInResult() {
+
+		}
+
+		[Fact]
+		public void GetField_ReturnedFieldWithTableName() {
+
+		}
+
+		[Fact]
+		public void GetFieldShort_ReturnedFieldWithoutTableName() {
+
+		}
+
+		[Fact]
+		public void GetFieldName_ReturnedFieldName() {
+
+		}
+
+		[Fact]
+		public void Select_ReturnedCommaseparatedFields() {
+
+		}
+
+		[Fact]
+		public void SelectInsert_ReturnedSqlInsertNotationFields() {
+
+		}
+
+		[Fact]
+		public void InsertOneField_ReturnedSqlInsertNotationOneField() {
+
+		}
+
+		[Fact]
+		public void Update_ReturnedSqlUpdateNotationFields() {
+
 		}
 	}
 }
