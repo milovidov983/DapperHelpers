7c26a1597b222898ddd9a8417e58aed7f34b85b1
Milovidov Vasya <milovidov.vasya@gmail.com>
2020-02-02 14:39:14 +0300

    all base tests

--- tests/TableUnitExtentionsTests/TableExtentionTest.cs
+++ tests/TableUnitExtentionsTests/TableExtentionTest.cs
@@ -40,47 +40,87 @@ namespace TableUnitExtentionsTests {
 
 		[Fact]
 		public void IncludeField_TheFieldIsIncludedInResult() {
+			var actual = db.UsersTable.Clear().Include(f => f.Email).Fields.OrderBy(x => x).ToArray();
 
+			var expected = new[] { nameof(Users.Email) };
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
 		public void ExcludeField_TheFieldIsExcludedInResult() {
+			var actual = db.UsersTable.Exclude(f => f.Email).Fields.OrderBy(x => x).ToArray();
 
+			var expected = typeof(Users).GetProperties()
+				.Select(prop => prop.Name)
+				.Where(x=>x != nameof(Users.Email))
+				.OrderBy(x => x)
+				.ToArray();
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
 		public void GetField_ReturnedFieldWithTableName() {
+			var actual = db.UsersTable.Field(f => f.Email);
 
+			var expected = $"\"{Users.TableName}\".\"{nameof(Users.Email)}\"";
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
 		public void GetFieldShort_ReturnedFieldWithoutTableName() {
+			var actual = db.UsersTable.FieldShort(f => f.Email);
 
+			var expected = $"\"{nameof(Users.Email)}\"";
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
 		public void GetFieldName_ReturnedFieldName() {
+			var actual = db.UsersTable.FieldName(f => f.Email);
 
+			var expected = nameof(Users.Email);
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
-		public void Select_ReturnedCommaseparatedFields() {
+		public void Select_ReturnedCommaseparatedFieldsWithTableName() {
+			var actual = db.UsersTable.Select();
 
+			var expected = @"""Users"".""Id"",""Users"".""Name"",""Users"".""Email"",""Users"".""CreatedAt""";
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
-		public void SelectInsert_ReturnedSqlInsertNotationFields() {
+		public void SelectInsert_ReturnedInsertSqlNotationFields() {
+			var actual = db.UsersTable.Insert();
 
+			var expected = @"@Id,@Name,@Email,@CreatedAt";
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
-		public void InsertOneField_ReturnedSqlInsertNotationOneField() {
+		public void InsertFromSelect_ReturnedInsertSqlNotationFields() {
+			var actual = db.UsersTable.SelectInsert();
 
+			var expected = @"""Id"",""Name"",""Email"",""CreatedAt""";
+
+			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
-		public void Update_ReturnedSqlUpdateNotationFields() {
+		public void Update_ReturnedUpdateSqlNotationFields() {
+			var actual = db.UsersTable.Update();
 
+			var expected = @"""Id""=@Id,""Name""=@Name,""Email""=@Email,""CreatedAt""=@CreatedAt";
+
+			Assert.Equal(expected, actual);
 		}
 	}
 }
